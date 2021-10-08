2aa85303226637e3ca68a123d2e2c33e06a0e822
kirito1219 <72923248+kirito1219@users.noreply.github.com>
2021-10-08 15:14:45 +0800

    Add files via upload

---
+++ project1/bonus_0856566.py
@@ -0,0 +1,34 @@
+from mininet.topo import Topo
+
+class Project1_Topo_0856566( Topo ):
+    def __init__( self ):
+        Topo.__init__( self )
+
+        # Add hosts
+        h1 = self.addHost( 'h1' , ip='192.168.0.1' )
+        h2 = self.addHost( 'h2' , ip='192.168.0.2' )
+        h3 = self.addHost( 'h3' , ip='192.168.0.3' )
+        h4 = self.addHost( 'h4' , ip='192.168.0.4' )
+
+
+        # Add switches
+        s1 = self.addSwitch( 's1' )
+        s2 = self.addSwitch( 's2' )
+        s3 = self.addSwitch( 's3' )
+        s4 = self.addSwitch( 's4' )
+        
+        # Add links
+        self.addLink( h1, s1 )
+        self.addLink( h2, s1 )
+        self.addLink( h3, s2 )
+        self.addLink( h4, s2 )
+        self.addLink( s3, s1 )
+        self.addLink( s4, s1 )
+        self.addLink( s3, s2 )
+        self.addLink( s4, s2 )
+
+        
+        
+
+
+topos = { 'topo_0856566': Project1_Topo_0856566 }
